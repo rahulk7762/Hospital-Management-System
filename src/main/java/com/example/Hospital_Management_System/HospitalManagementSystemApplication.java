7184ba3fb9b9e90baca6c9aa5f8baade52146549
rahul-kumar25 <rahul.kumar@clivertech.com>
2026-09-04 10:25:42 +0530

    Hospital Management System SpringBoot Project

---
+++ src/main/java/com/example/Hospital_Management_System/HospitalManagementSystemApplication.java
@@ -0,0 +1,17 @@
+package com.example.Hospital_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+		
+		
+		System.out.println("SptingBoot Application Started");
+		
+	}
+
+}
